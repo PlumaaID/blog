52bb50c2ca69e4cc65d0eb43ea1d5995c13a9df2
ernestognw <ernestognw@gmail.com>
2024-04-29 18:42:07 -0600

    Customize

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,11 +9,11 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
-	shareMessage: 'Share this post', // Message to share a post on social media
+	author: 'Ernesto García', // Site author
+	title: 'Pluma ID - Blog', // Site title.
+	description: 'Firma Electrónica Avanzada y Tecnología Financiera', // Description to display in the meta tags
+	lang: 'es-mx',
+	ogLocale: 'ex_MX',
+	shareMessage: 'Compartir', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
